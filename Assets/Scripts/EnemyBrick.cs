f6f04e424d8c466c51770a2c27d86152382be03e
mkatic <michaelkatic@gmail.com>
2019-01-26 00:44:32 -0700

    Added some starter scripts/structuring and basic nav mesh agents.

---
+++ Assets/Scripts/EnemyBrick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBrick : MonoBehaviour
+{
+	private NavMeshAgent navMeshAgent;
+	private BrickTower brickTower;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		brickTower = FindObjectOfType<BrickTower>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		navMeshAgent.SetDestination(brickTower.transform.position);
+	}
+}

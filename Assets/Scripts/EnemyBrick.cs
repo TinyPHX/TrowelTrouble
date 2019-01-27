680e435927a94f57ea93c366f58df5042994354f
mkatic <michaelkatic@gmail.com>
2019-01-27 13:13:29 -0700

    Added swapping dead bricks to the dead model. Added all brick models and animations.

--- Assets/Scripts/EnemyBrick.cs
+++ Assets/Scripts/EnemyBrick.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.VR.WSA.Input;
+using Random = UnityEngine.Random;
 
 public class EnemyBrick : MonoBehaviour
 {
@@ -10,24 +13,70 @@ public class EnemyBrick : MonoBehaviour
     public bool isDead = false;
     public bool inTower = false;
     public int respawnCount = 0;
+    public float maxSpeed = 1f;
+    public float invincibleTime = 1;
+
+    [Header(" --- Models --- ")]
+    [SerializeField] private GameObject aliveModel;
+    [SerializeField] private GameObject deadModel;
+    
+    [Header(" --- Animation --- ")]
+    [SerializeField] private Animator characterAnimator;
+    [SerializeField, ReadOnly] private const string ANIM_SPEED_MULTIPLIER = "SpeedMultiplier";
+    [SerializeField, ReadOnly] private const string ANIM_GROUNDED = "Grounded";
+    [SerializeField, ReadOnly] private const string ANIM_JUMP = "Jump";
+    [SerializeField, ReadOnly] private const string ANIM_ATTACK = "Attack";
 
     [Header(" --- Other --- ")]
-    public NavMeshAgent navMeshAgent;
-
-    public new Rigidbody rigidbody;
+    [SerializeField, ReadOnly] public NavMeshAgent navMeshAgent;
+    [SerializeField, ReadOnly] public new Rigidbody rigidbody;
     private BrickTower brickTower;
     private GameObject cube;
+    private float lastHitTime;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Collider aliveHitBox;
+    [SerializeField] private Collider deadHitBox;
+    [SerializeField, ReadOnly] private bool grounded;
+    [SerializeField, ReadOnly] private bool previousGrounded;
+    public Holdable holdable;
+
+    public bool IsHeld
+    {
+        get { return holdable.BeingHeld; }
+    }
 
     public void Hit()
     {
         if (this.health > 0)
         {
-            this.health -= 1;
+            if (Time.time - lastHitTime > invincibleTime)
+            {
+                this.health -= 1;
+                lastHitTime = Time.time;
+            }
         }
+        
         if (this.health == 0 && this.isDead == false)
         {
             this.isDead = true;
+            Die();
         }
+    }
+
+    public void Die()
+    {
+        aliveModel.SetActive(false);
+        deadModel.SetActive(true);
+        rigidbody.mass = 5;
+        navMeshAgent.speed *= .1f;
+    }
+
+    public void PickedUp()
+    {
+        holdable.BeingHeld = true;
+        navMeshAgent.enabled = false;
+
+        Debug.Log("PickedUp");
     }
 
     public void ResetToDefaults()
@@ -50,6 +99,12 @@ public class EnemyBrick : MonoBehaviour
     // Use this for initialization
     public void Start()
     {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        rigidbody = GetComponent<Rigidbody>();
+        brickTower = FindObjectOfType<BrickTower>();
+        holdable = GetComponent<Holdable>();
+        
+        navMeshAgent.speed = maxSpeed;
     }
 
     public void Unstack()
@@ -61,9 +116,83 @@ public class EnemyBrick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (navMeshAgent != null)
+        if (navMeshAgent != null && navMeshAgent.enabled && !IsHeld && grounded)
         {
             navMeshAgent.SetDestination(brickTower.transform.position);
+
+            if (!isDead)
+            {
+                
+            }
         }
+
+        UpdateAnimation();
+        
+        CheckIfGrounded();
+        
+        EnableDisableNavAgent();
+    }
+
+    private void EnableDisableNavAgent()
+    {
+        if (grounded && !IsHeld && !inTower && !navMeshAgent.enabled)
+        {
+//            navMeshAgent.pos
+            navMeshAgent.enabled = true;
+        }
+    }
+	
+    private void CheckIfGrounded()
+    {
+        previousGrounded = grounded;
+        
+        bool groundCheck = false;
+        float distanceToGround = .02f;
+
+        RaycastHit raycastHit = new RaycastHit();
+        groundCheck = Physics.Raycast(
+            new Ray(HitBox.bounds.center, Vector3.down),
+            out raycastHit,
+            HitBox.bounds.size.y / 2 + distanceToGround,
+            groundMask);
+
+        grounded = groundCheck;
+    }
+
+    private void UpdateAnimation()
+    {
+        if (!inTower)
+        {
+            float speedRatio = maxSpeed / navMeshAgent.velocity.magnitude;
+            characterAnimator.SetFloat(ANIM_SPEED_MULTIPLIER, speedRatio);
+        }
+    }
+    
+    private void MoveRandomDirection(float distance)
+    {
+        Vector3 randomPositionOffset = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f)) * distance;
+        navMeshAgent.SetDestination(transform.position + randomPositionOffset);
+    }
+
+    private void MoveTo(Vector3 position)
+    {
+        navMeshAgent.SetDestination(position);
+    }
+
+    private void MoveAwayFrom(Vector3 position, float distance)
+    {
+        Vector3 direction = (transform.position - position).normalized;
+        navMeshAgent.SetDestination(transform.position + (direction * distance));
+    }
+
+    private void StopMoving()
+    {
+        navMeshAgent.SetDestination(transform.position);
+        navMeshAgent.isStopped = true;
+    }
+
+    private Collider HitBox
+    {
+        get { return isDead ? deadHitBox : aliveHitBox; }
     }
 }
---
+++ Assets/Scripts/PlayerAnimatorEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorEvents : MonoBehaviour
+{
+	[SerializeField] private PlayerController player;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Slash() 
+	{
+		Debug.Log("slash");
+		player.TrowelSlashStart();
+	}
+	
+	public void SlashEnd() 
+	{
+		Debug.Log("slash");
+		player.TrowelSlashEnd();
+	}
+}
